86a087480b7a34a50796076e8eed58a4858c9a92
Renan Peixoto <renan2.peixoto@gmail.com>
2023-08-31 21:10:10 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardDetailComponent } from './components/card-detail/card-detail.component';
+import { CardAboutComponent } from './components/card-about/card-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    CardDetailComponent,
+    CardAboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-about/card-about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-about',
+  templateUrl: './card-about.component.html',
+  styleUrls: ['./card-about.component.css']
+})
+export class CardAboutComponent {
+
+}
